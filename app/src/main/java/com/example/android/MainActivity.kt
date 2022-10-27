2acee167ab51e118207850166c6165931e3aaf48
Spirit-Hat <vladonline34@gmail.com>
2022-10-27 22:56:15 +0300

    lab1-3

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -6,16 +6,29 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this@MainActivity, "onCreate()",
 
-        val clickme = findViewById<Button>(R.id.button1)
+            Toast.LENGTH_LONG).show()
+    }
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity, "onPause()",
 
-        clickme.setOnClickListener{
+            Toast.LENGTH_LONG).show()
+    }
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this@MainActivity, "onStart()",
 
-            Toast.makeText(this,"Avramchuk Vlad",Toast.LENGTH_SHORT).show()
-        }
+            Toast.LENGTH_LONG).show()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this@MainActivity, "onRestart()",
+
+            Toast.LENGTH_LONG).show()
     }
 }
